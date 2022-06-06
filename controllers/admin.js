7fc9c2b046d17de0f51c6c889b6e7bbf60bb6297
Saugat Poudel <njsaugat@gmail.com>
2022-06-06 07:33:17 +0545

    admin file added

---
+++ controllers/admin.js
@@ -0,0 +1,104 @@
+const Product=require('../models/productData')
+const getAddProduct=(req,res)=>{
+    // res.sendFile(path.join(rootDirectory,'/views/add-product.html'))without using templating engine
+    res.render('admin/add-product',{
+        docTitle:'Add Product',
+        formsCSS:true,
+        productCSS:true,
+        activeAddProduct:true
+    })
+}
+
+
+const postAddProduct=(req,res,next)=>{
+    const title=req.body.title;
+    const imageUrl=req.body.imageUrl;
+    const price=req.body.price;
+    const description=req.body.description; 
+    const product=new Product(title,imageUrl,price, description)
+    product
+        .save()
+        .then(result=>{
+            console.log('product created')
+            res.redirect('/admin/products')
+        })
+    // req.user
+    //     .createProduct({//like yo chai tyo relation vako hunale+ tyo name ma Product chai Product vanne obj bata ako 
+    //         title,price,imageUrl,description
+    //         })
+    //     .then(result=>{
+    //         console.log('Product Created')
+    //         res.redirect('/') 
+    //     })    
+    //     .catch(err=>console.log(err))
+
+}
+
+const getProductsAdmin=(req,res,next)=>{
+    Product.fetchAll()
+    .then(products=>{
+        res.render('admin/products-admin',{
+            products,
+            hasProducts:products.length>0,
+            docTitle:'Admin Products'
+        })
+    })
+    .catch(err=>console.log(err))   
+}
+
+const editProduct=(req,res,next)=>{
+    Product.findById(req.params.id)
+    .then(product=>{
+        res.render('admin/edit-product',{
+            finalProduct:product,
+            docTitle:'Edit Products',
+        }) 
+    })
+    .catch(err=>console.log(err))
+    
+}
+
+
+const postProduct=(req,res,next)=>{
+    const editObject={ 
+        id:req.params.id,
+        title:req.body.title,
+        imageUrl:req.body.imageUrl,
+        price:req.body.price,
+        description:req.body.description
+
+    }
+    Product.update(editObject,req.params.id)
+    .then(product=>{
+        if(product){
+            return res.redirect('/');
+        }
+    })
+    
+    // Product.findByPk(req.params.id)
+    // Product.update(editObject,
+    //     {where: {id:req.params.id}}
+    // )
+    // .then(()=>res.redirect('/'))
+    // .catch(err=>console.log(err))
+    
+    
+}
+
+// const deleteProduct=(req,res,next)=>{
+//     Product.destroy({where:{id:req.params.id}})
+//     .then(()=>res.redirect('/'))
+//     .catch(err=>console.log(err))
+
+    
+// }
+//
+module.exports={
+    getAddProduct,
+    postAddProduct,
+    getProductsAdmin,
+    editProduct,
+    postProduct,
+    // deleteProduct
+}
+
