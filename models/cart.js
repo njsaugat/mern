0797672ee59373c470ec54c49ab45c74d6a821e7
Saugat Poudel <njsaugat@gmail.com>
2022-06-07 07:26:45 +0545

    cart file added

---
+++ models/cart.js
@@ -0,0 +1,14 @@
+const Sequelize=require('sequelize');
+
+const sequelize=require('../utils/database');
+
+const Cart=sequelize.define('cart',{
+    id:{
+        type:Sequelize.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
+    },
+
+})
+
+module.exports=Cart;
