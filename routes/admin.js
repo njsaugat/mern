6d5449c80b711b3fe53903921c189423e37d97e0
Saugat Poudel <njsaugat@gmail.com>
2022-06-07 07:29:19 +0545

    admin route added

---
+++ routes/admin.js
@@ -0,0 +1,25 @@
+const express=require('express')
+const path=require('path')
+const router=express.Router();//like routing ko lagi special
+const rootDirectory=require('../utils/pathFinder')
+const adminController=require('../controllers/admin');
+const { route } = require('express/lib/application');
+const { rmSync } = require('fs');
+const { runInContext } = require('vm');
+const req = require('express/lib/request');
+
+router.get('/add-product',adminController.getAddProduct)
+// app.post('/message',parser,(req,res)=>{//body-parser package install garera garyo vane function bhitra pass garna pardaina
+
+router.post('/add-product',adminController.postAddProduct)
+
+router.get('/products',adminController.getProductsAdmin)
+
+router.get('/edit-product/:id',adminController.editProduct)
+
+router.post('/products/:id',adminController.postProduct)
+
+router.post('/delete-product/:id',adminController.deleteProduct)
+
+// router.get('/products/:id',adminController.editProduct)
+module.exports={router};
