051b0007448d95179d901e84d2b48d1ab50290ab
Saugat Poudel <njsaugat@gmail.com>
2022-06-10 07:14:27 +0545

    order file added

---
+++ models/order.js
@@ -0,0 +1,28 @@
+const { ObjectId } = require('mongodb')
+const mongoose=require('mongoose')
+
+const Schema=mongoose.Schema
+
+const orderSchema =new Schema({
+    products:[
+        {
+            product:{type:Object,
+                required:true,
+                ref:'Product'},
+            quantity:{type:Number,required:true}
+        }
+    ],
+    user:{
+        name:{
+            type:String,
+            required:true
+        },
+        userId:{
+            type:Schema.Types.ObjectId,
+            required:true,
+            ref:'User'
+        }
+    }
+})
+
+module.exports=mongoose.model('Order',orderSchema)
