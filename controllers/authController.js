4858717fd094306d272e69dfe1735ada169b6f4b
Saugat Poudel <njsaugat@gmail.com>
2022-06-11 12:34:57 +0545

    auth file added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,23 +1,41 @@
+const User = require("../models/user")
+
 const getLogin=(req,res,next)=>{
     // const isLoggedIn=req.get('Cookie').trim().split('=')[1]
     const isLoggedIn=true
     console.log(req.session.isLoggedIn)
     res.render('auth/login',{
         docTitle:'Login',
-        isAuthenticated:isLoggedIn
+        isAuthenticated:req.session.isLoggedIn
     })
 }
 
 const postLogin=(req,res,next)=>{
     // res.setHeader('Set-Cookie','loggedIn=true;')//loggedIn=true; Expires= new Date.now() ; Max-age= 10; Secure;HttpOnly
+    // req.isLoggedIn=true;
     
     //session:
-    req.session.isLoggedIn=true
+    User.findById('62a0a9a9e6247f61284f23dc')
+        .then(user=>{
+            req.session.isLoggedIn=true
+            req.session.user=user;
+            req.session.save((err)=>{//making sure we save to db and then redirect ie making it async
+                console.log(err)
+                res.redirect('/')
+            })
+        })
+        .catch(err=>console.log(err))
 
-    // req.isLoggedIn=true;
-    res.redirect('/')
 }
+const postLogout=(req,res,next)=>{
+    req.session.destroy((err)=>{
+        console.log(err)
+        res.redirect('/')
+    })
+}
+
 module.exports={
     getLogin,
-    postLogin
+    postLogin,
+    postLogout
 }
