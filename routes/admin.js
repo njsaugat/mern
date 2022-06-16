41a353f05afb4adfd0399fd1c6f389817ca5188f
Saugat Poudel <njsaugat@gmail.com>
2022-06-16 07:43:51 +0545

    admi n routes updated

--- routes/admin.js
+++ routes/admin.js
@@ -8,18 +8,62 @@ const { rmSync } = require('fs');
 const { runInContext } = require('vm');
 const req = require('express/lib/request');
 
-router.get('/add-product',adminController.getAddProduct)
-// app.post('/message',parser,(req,res)=>{//body-parser package install garera garyo vane function bhitra pass garna pardaina
+const {body}=require('express-validator')
+const isAuth=require('../middleware/isAuth')
 
-router.post('/add-product',adminController.postAddProduct)
+router.get('/add-product',isAuth,adminController.getAddProduct)
+// // app.post('/message',parser,(req,res)=>{//body-parser package install garera garyo vane function bhitra pass garna pardaina
 
-router.get('/products',adminController.getProductsAdmin)
+router.post('/add-product',
+    [
+        body('title')
+            .isString()
+            .isLength({min:3})
+            .trim(),
+        // body('imageUrl')
+        //     .isURL(),
+        body('price')
+            .isFloat(),
+        body('description')
+            .isLength({min:5, max:400})
+            .trim()
+    ]
+    ,
+    isAuth,adminController.postAddProduct)
 
-router.get('/edit-product/:id',adminController.editProduct)
+router.get('/products',isAuth,adminController.getProductsAdmin)
 
-router.post('/products/:id',adminController.postProduct)
+router.get('/edit-product/:id',
+        [
+        body('title')
+            .isString()
+            .isLength({min:3})
+            .trim(),
+        // body('imageUrl')
+        //     .isURL(),
+        body('price')
+            .isFloat(),
+        body('description')
+            .isLength({min:5, max:400})
+            .trim()
+        ],
+        isAuth,adminController.editProduct)
 
-router.post('/delete-product/:id',adminController.deleteProduct)
+router.post('/products/:id',
+    [
+        body('title')
+            .isAlphanumeric()
+            .isLength({min:3})
+            .trim(),
+        body('imageUrl').isURL(),
+        body('price').isFloat(),
+        body('description')
+            .isLength({min:5, max:400})
+            .trim()
+    ],
+    isAuth,adminController.postProduct)
+
+router.post('/delete-product/:id',isAuth,adminController.deleteProduct)
 
 // router.get('/products/:id',adminController.editProduct)
 module.exports={router};
