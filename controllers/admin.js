3ee36979fe1a128f32e380b291d5037e2c93852c
Saugat Poudel <njsaugat@gmail.com>
2022-06-18 17:52:59 +0545

    admin file updated

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,17 +1,48 @@
 const Product=require('../models/productData')
+const {validationResult}=require('express-validator')
+
+const fileHelper=require('../utils/file')
 const getAddProduct=(req,res)=>{
     res.render('admin/add-product',{
         docTitle:'Add Product',
-        isAuthenticated:req.session.isLoggedIn
+        isAuthenticated:req.session.isLoggedIn,
+        finalProduct:{
+            title:'',
+            imageUrl:'',
+            price:'',
+            description:''
+        },
+        errorMessage:''
     })
 }
 
 
 const postAddProduct=(req,res,next)=>{
     const title=req.body.title;
-    const imageUrl=req.body.imageUrl;
+    const image=req.file;
     const price=req.body.price;
     const description=req.body.description; 
+    const errors=validationResult(req)
+
+    if(!image){
+        return res.status(422).render('admin/add-product',{
+            finalProduct:{title,image,price,description},
+            docTitle:'Add Product',
+            isAuthenticated:req.session.isLoggedIn,
+            errorMessage:'Attached file is not an image',
+        })
+    }
+    if(!errors.isEmpty()){
+        return res.status(422).render('admin/add-product',{
+            finalProduct:{title,image,price,description},
+            docTitle:'Add Product',
+            isAuthenticated:req.session.isLoggedIn,
+            errorMessage:errors.array()[0].msg,
+     
+        })
+    }
+
+    const imageUrl=image.path;
     const product=new Product({
         title:title,
         imageUrl:imageUrl,
@@ -27,7 +58,7 @@ const postAddProduct=(req,res,next)=>{
 }
 
 const getProductsAdmin=(req,res,next)=>{
-    Product.find()
+    Product.find({userId:req.user._id})
     // .select('title price -_id  ')//selecting and populating certain fields
     // .populate('userId','name')
     .then(products=>{
@@ -42,12 +73,35 @@ const getProductsAdmin=(req,res,next)=>{
 }
 
 const editProduct=(req,res,next)=>{
+    // const title=req.body.title;
+    // const imageUrl=req.body.imageUrl;
+    // const price=req.body.price;
+    // const description=req.body.description;
+    const title=req.body.title;
+    const image=req.file;
+    const price=req.body.price;
+    const description=req.body.description; 
+    const errors=validationResult(req)
+
+    
     Product.findById(req.params.id)
     .then(product=>{
+        if(!errors.isEmpty()){
+            return res.status(422).render('admin/edit-product',{
+                finalProduct:product,
+                docTitle:'Edit Product',
+                isAuthenticated:req.session.isLoggedIn,
+                errorMessage:errors.array()[0].msg,
+         
+            })
+        }
+        console.log(req.file)
+        console.log(product)
         res.render('admin/edit-product',{
             finalProduct:product,
             docTitle:'Edit Products',
-            isAuthenticated:req.session.isLoggedIn
+            isAuthenticated:req.session.isLoggedIn,
+            errorMessage:null
         }) 
     })
     .catch(err=>console.log(err))
@@ -56,12 +110,16 @@ const editProduct=(req,res,next)=>{
 
 
 const postProduct=(req,res,next)=>{
+    const image=req.file;
     const editObject={ 
         id:req.params.id,
         title:req.body.title,
-        imageUrl:req.body.imageUrl,
+        // imageUrl:req.body.imageUrl,
         price:req.body.price,
         description:req.body.description
+    }
+    if(image){
+        editObject.imageUrl=image.path
     }
     Product.updateOne({_id:req.params.id},{...editObject})
     .then(product=>{
@@ -72,8 +130,18 @@ const postProduct=(req,res,next)=>{
 }
 
 const deleteProduct=(req,res,next)=>{
-    Product.deleteOne({_id:req.params.id})
-    .then(()=>res.redirect('/'))
+    Product.findById(req.params.id)
+    .then(product=>{
+        if(!product){
+            console.log('No product found')
+        }
+        fileHelper.deleteFile(product.imageUrl)
+        return Product.deleteOne({_id:req.params.id})
+    })
+    .then(()=>{
+        res.redirect('/')
+        
+    })
     .catch(err=>console.log(err))
 
     
